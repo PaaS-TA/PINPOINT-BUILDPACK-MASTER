121a22cb641ea99e4414fd5fd19f31dc0b67e46a
aromkimjinsu <53206912+aromkimjinsu@users.noreply.github.com>
2019-12-06 16:34:58 +0900

    Update package.rb

--- rakelib/package.rb
+++ rakelib/package.rb
@@ -37,6 +37,6 @@ module Package
 
   STAGING_DIR = "#{BUILD_DIR}/staging"
 
-  PACKAGE_NAME = "#{BUILD_DIR}/java-buildpack#{offline}-#{version}.zip"
+  PACKAGE_NAME = "#{BUILD_DIR}/java-buildpack-pinpoint-monitoring-#{version}.zip"
 
 end
