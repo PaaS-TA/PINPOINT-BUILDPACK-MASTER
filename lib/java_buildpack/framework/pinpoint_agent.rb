59f04fb4a54bee82f828a00dee4cd7a9163347b8
heegeunyoon <nowruri@gmail.com>
2020-04-27 17:38:09 +0900

    Update pinpoint_agent.rb

--- lib/java_buildpack/framework/pinpoint_agent.rb
+++ lib/java_buildpack/framework/pinpoint_agent.rb
@@ -80,7 +80,7 @@ module JavaBuildpack
         collector_tcp_port = credentials['collector_tcp_port']
         #agent_id = credentials['agent_id']
         #agent_id = SecureRandom.urlsafe_base64
-        application_name = credentials['application_name'] || @application.details['name']
+        application_name = credentials['application_name'] || @application.details['uris'][0]
 
         [collector_host, collector_span_port, collector_stat_port, collector_tcp_port, application_name]
       end
